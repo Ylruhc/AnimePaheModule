a1a5e8f6c1f1530d1dbd57ae34840a868846dfa8
Ylruhc <osmandawud91@gmail.com>
2025-04-04 02:31:34 +0100

    Update AnimePahe.js

--- AnimePahe.js
+++ AnimePahe.js
@@ -118,6 +118,7 @@ async function searchResults(keyword) {
       const response = await fetchv3(`${ SEARCH_URL }/?m=search&q=${ encodeURIComponent(keyword) }`);
         console.error('afterFetch');
       //const html = typeof response === 'object' ? await response.text() : await response; // Website response (Pick only one, both will give an error)
+    console.error(response)
       const data = await JSON.parse(response); // API response (Pick only one, both will give an error)
       const formatted_response = data['data'].map((x)=>{return {title:x['title'],image:x['poster'],href:`${x['session']}`}})
       return JSON.stringify(formatted_response);
