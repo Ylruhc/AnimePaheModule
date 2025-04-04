030e1c0360f26cf1220cae661ca35abc04afae37
Ylruhc <osmandawud91@gmail.com>
2025-04-04 11:39:48 +0100

    Update AnimePahe.js

--- AnimePahe.js
+++ AnimePahe.js
@@ -123,7 +123,7 @@ async function searchResults(keyword) {
     console.error(typeof response)
         console.error("BEFORE PARSE")
       const data = await JSON.parse(response); // API response (Pick only one, both will give an error)
-    const body = data['body'];
+    const body = await JSON.parse(data['body']);
     console.error("AFTER PARSE")
     console.error(data)
       const formatted_response = body['data'].map((x)=>{return {title:x['title'],image:x['poster'],href:`${x['session']}`}})
