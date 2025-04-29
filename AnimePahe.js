57b85ea3acfbd17717618def7d03d00d5a3ec26c
Ylruhc <osmandawud91@gmail.com>
2025-04-29 11:34:25 +0100

    Update AnimePahe.js

--- AnimePahe.js
+++ AnimePahe.js
@@ -211,7 +211,7 @@ async function extractStreamUrl(url) {
      // console.error("REDIRECT URL IS")
       //console.error(redirectUrl)
       const streamUrl = await fetchDownloadLink(redirectUrl)
-      return streamUrl
+      return {stream:{streamUrl:streamUrl,headers:{Referer:"kwik.cz",Origin:"kwik.cz"}}}
       // const html = typeof response === 'object' ? await response.text() : await response; // Website response (Pick only one, both will give an error)
       // const data = typeof response === 'object' ? await response.json() : await JSON.parse(response); // API response (Pick only one, both will give an error)
       //console.error("STREAM URL IS")
